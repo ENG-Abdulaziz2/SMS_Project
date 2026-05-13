1a79d88c282479f8310c599bd9ab58212637fabb
ENG-Abdulaziz2 <azezalabdan@gmail.com>
2026-05-13 21:55:56 +0300

    Add files via upload

---
+++ DatabaseManager.java
@@ -0,0 +1,21 @@
+package test;
+
+public class DatabaseManager {
+
+    String username = "admin";
+    String password = "admin123";
+
+    public void connect() {
+
+        int a = 10;
+        int b = 10;
+
+        if (a == b) {
+            System.out.println("Connected");
+        }
+
+        if (a == b) {
+            System.out.println("Connected");
+        }
+    }
+}
---
+++ UserService.java
@@ -0,0 +1,15 @@
+package test;
+
+public class UserService {
+
+    public void printUsers() {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("User");
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("User");
+        }
+    }
+}
